89607d188b941d092e2ae471ce6b9218d9e7724e
Vonetta <vonettastevenson@gmail.com>
2017-11-11 17:22:41 -0800

    adding palindrome algorithm

---
+++ htmlpage/htmlpage/palidrome/script.js
@@ -0,0 +1,13 @@
+﻿var array = [];
+
+function palindrome(str) {
+    for (var i = str.length -1; i >= 0; i--) {
+        console.log(str[i ]);
+        array.push(str[i])
+    }
+        console.log(array)
+}
+
+palindrome("max");
+
+
